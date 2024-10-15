e3d6ff962e8ef0dca9a674677141dd823556480b
Kevin Rexach <kaiorex@hotmail.com>
2024-10-15 12:07:24 -0400

    Add project files.

---
+++ Arrays and Structs Programming Project.cpp
@@ -0,0 +1,11 @@
+// Arrays and Structs Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Arrays and Structs.\n";
+}
+
+
